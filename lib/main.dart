f63fbeb03f7803ad6ae9b1cdefb957dbd079fcb1
[ahmadous] <[as149045@gmail.com]>
2023-10-14 17:01:31 +0000

    add assets to doc images

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:studentbankmobile/widget/theme_Provider.dart';
+import 'package:studentbankmobile/widgets/theme_Provider.dart';
 
 void main() {
   runApp(const MyApp());
