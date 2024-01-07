d96f2f17a13076b87a9a577f077c9f206ac47f8f
ahmadous <as149045@gmail.com>
2024-01-07 03:35:52 +0000

    petite changement sur le main

---
+++ lib/banque_sow/dark/bank2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bank2 extends StatefulWidget {
+  const Bank2({super.key});
+
+  @override
+  State<Bank2> createState() => _Bank2State();
+}
+
+class _Bank2State extends State<Bank2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
